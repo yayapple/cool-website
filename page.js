b5cccd28bb364326b52858f1aec07c16d1fc9009
ethan <applecyn1@gmail.com>
2021-06-30 17:25:33 -0500

    added javascript for footer and message

---
+++ page.js
@@ -0,0 +1,47 @@
+var date = new Date();
+var day = date.getDay();
+function happyDate() {
+	const weekdays = new Array("sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday");
+  document.getElementById("message-section").firstElementChild.innerHTML = "happy " + weekdays[day];
+}
+happyDate();
+
+function footerText() {
+  const messages = new Array(
+    "give me your credit card information please.",
+    "this has been an ethan™ experience.",
+    "i love you.",
+    "let's keep in touch.",
+    "please give me your mom's phone number.",
+    "tell me a joke.",
+    "don't be surprised.",
+    "i'm behind you.",
+    "turn around.",
+    "lol cat..",
+    "who wants to play valorant.",
+    "i know your address.",
+    "stop posting about among us.",
+    "keep it to yourself.",
+    "reload the page.",
+    "i don't know what to put here.",
+    "dave please change the footer text thank you.",
+    "get owned.",
+    "dbjnhiyunkzvdrcdggkuywuntmnarsto.",
+    "i'm in me mum's car.",
+    "so... are we like a thing?..",
+    "i need to know.",
+    "send me your finest money.",
+    "i also do website and carrd commissions.",
+    "this text changes you know.",
+    "we already know. it's ok. we forgive you.",
+    "mom, dad, i'm gay.",
+    "you're just as funny as you think you are.",
+    "i also tutor people.",
+    "i'm low on cocaine.",
+    "you have to do it. you need to.",
+    "you're out of eggs.",
+    "go drink some water."
+  );
+  document.getElementById("footer").firstElementChild.innerHTML = messages[messages.length * Math.random() | 0];
+}
+footerText();
