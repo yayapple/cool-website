abbd18bcd4da7ca526f8ad8f9b5eed97b8611d18
ethan <empmn1@gmail.com>
2021-07-07 13:43:26 -0500

    fixed js loading

--- page.js
+++ page.js
@@ -2,16 +2,15 @@ happyDate();
 footerText();
 
 var xhr = new XMLHttpRequest();
+xhr.open("GET", "https://api.countapi.xyz/hit/amongus-s.us/hits");
+xhr.send();
+
 xhr.open("GET", "https://api.countapi.xyz/get/amongus-s.us/clicks");
 xhr.responseType = "json";
 xhr.onload = function() {
   document.getElementById("clickbutton").innerHTML = this.response.value;
 }
 xhr.send();
-
-xhr.open("GET", "https://api.countapi.xyz/hit/amongus-s.us/hits");
-xhr.send();
-
 
 function happyDate() {
   const date = new Date();
